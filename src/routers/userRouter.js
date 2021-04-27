02165ef4ab052dd308591b401e58a0d2ceadd145
samueljade <sbferrer3@up.edu.ph>
2021-04-27 14:27:56 +0800

    Add user log out route

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const router = new express.Router;
 const User = require("../models/user.js");
+const auth = require("../middleware/auth.js");
 
 router.post("/users/register", async (req, res) => {
   const user = new User(req.body);
@@ -34,8 +35,30 @@ router.post("/users/login", async (req, res) => {
   } catch(error) {
     console.log(error);
     res.status(400).send({message:"Wrong username or password"});
+  }   
+});
+
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+
+    // Delete current token
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    
+    await req.user.save();
+    res.status(200).send();
+
+  } catch(error) {
+    console.log(error);
+    res.status(500).send()
   }
-   
+
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  // Sample route for auth
+  res.send(req.user);
 });
 
 module.exports = router;
