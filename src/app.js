68a1597d03a9309d6d744a739f91939334df4ce8
samueljade <sbferrer3@up.edu.ph>
2021-04-27 12:58:54 +0800

    Add db conn and userRouter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const userRouter = require("./routers/userRouter.js")
 const app = express();
+require("./db/connection.js");
+app.use(express.json());
+app.use(userRouter);
 
 app.get("/", (req, res) => {
   res.status(200).send({message: "OK"});
