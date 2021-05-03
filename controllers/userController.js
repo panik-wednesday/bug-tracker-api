2fa39023c773da99986d61b4329af050c76aae66
samueljade <sbferrer3@up.edu.ph>
2021-05-03 19:31:17 +0800

    Fix user login sending msg in 400 status

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,7 @@ async function login(req, res) {
   
   } catch(error) {
     console.log(error);
-    res.status(400).send({message:"Wrong username or password"});
+    res.status(400).send();
   }   
 }
 
