77614fb6c9988d75857eecfd33a71270c4747c0c
samueljade <sbferrer3@up.edu.ph>
2021-05-18 14:32:02 +0800

    Add trial for conflict

--- router.js
+++ router.js
@@ -10,8 +10,11 @@ router.post("/users/register", User.create);
 router.post("/users/login", User.login);
 router.post("/users/logout", auth, User.logout);
 router.get("/users/me", auth, User.me);
+router.get("/users/test", auth, User.me);
+router.get("/users/test2", auth, User.me);
 
 // Bug routes
+router.get("/bugs/test", auth, Bug.getAll);
 router.get("/bugs", auth, Bug.getAll);
 router.get("/bugs/:id", auth, Bug.getOne);
 router.post("/bugs", auth, Bug.create);
